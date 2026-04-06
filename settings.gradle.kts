287f121a454f2c7ceef00b4de39cca2f1246a89e
utzcoz <utzcoz@outlook.com>
2026-04-06 16:28:51 +0800

    build: convert settings.gradle to Kotlin DSL

---
+++ settings.gradle.kts
@@ -0,0 +1,40 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "hello-digitalis"
+include(":hello-vulkan")
+include(":hello-jni")
+include(":hello-jniCallback")
+include(":exceptions")
+include(":bitmap-plasma")
+include(":hello-gl2")
+include(":gles3jni")
+include(":native-activity")
+include(":native-audio")
+include(":native-codec")
+include(":native-midi")
+include(":sensor-graph")
+include(":camera-basic")
+include(":camera-texture-view")
+include(":teapots-classic")
+include(":teapots-more")
+include(":teapots-textured")
+include(":endless-tunnel")
+include(":sanitizers")
+include(":unit-test")
+include(":vectorization")
+include(":orderfile")
+include(":screenshot-test-lib")
